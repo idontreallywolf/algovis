4409257534273f7644abb8d24dd0c91673e016b9
idontreallywolf <nedim.kanat1999@gmail.com>
2020-06-21 17:32:03 +0200

    add constants

---
+++ public/js/const.js
@@ -0,0 +1,8 @@
+const NODE_COLOR_OBSTACLE    = "#000";
+const NODE_COLOR_EMPTY       = "#fff";
+const NODE_COLOR_DESTINATION = "#0f0";
+
+const TOOL_FREENODE    = 0;
+const TOOL_OBSTACLE    = 1;
+const TOOL_DESTINATION = 2;
+const TOOL_MOVENODE    = 3;
